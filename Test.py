2b53aaf535c7bc51a034b1b2f090b0d0bebb0642
abdalrhmanalsaid1223 <abdalrhmanalsaid1223@gmail.com>
2026-07-28 02:25:13 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+temp = float(input("Enter the temperature: "))
+unit = input("Enter the unit of temperature (C for Celsius, F for Fahrenheit): ")
+
+if unit == "C":
+    temp = round(1.8 * temp + 32, 2)
+    unit = "F"
+    print(f"temperature is {temp} {unit}")
+
+elif unit == "F":
+    temp = round((temp - 32) / 1.8, 2)
+    unit = "C"
+    print(f"temperature is {temp} {unit}")
+
+else:
+    print(f"{unit} is not a unit")
